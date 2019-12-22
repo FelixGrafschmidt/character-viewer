2e75859987d07890a3d3e1cedab6607d58addb31
Ithambar <drache.dragon@gmail.com>
2019-12-22 12:54:46 +0100

    enable production builds

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 // vue.config.js
 module.exports = {
 	"outputDir": "./target",
-	publicPath: process.env.NODE_ENV === 'production'
-		? '/character-viewer/'
-		: '/',
+	// publicPath: process.env.NODE_ENV === 'production'
+	// 	? '/character-viewer/'
+	// 	: '/',
 	devServer: {
 		proxy: {
 			"/saveList": {
