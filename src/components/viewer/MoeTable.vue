b6877d19ad8acd9163f68b669fc41a7587b3432d
Ithambar <drache.dragon@gmail.com>
2020-04-05 08:48:14 +0200

    enlarge image

--- src/components/viewer/MoeTable.vue
+++ src/components/viewer/MoeTable.vue
@@ -8,7 +8,12 @@
 				<b-table-column field="origin" label="Origin" width="700" sortable searchable>
 					{{ props.row.origin }}
 				</b-table-column>
-				<b-table-column field="imageurl" label="Image" width="40">
+				<b-table-column
+					field="imageurl"
+					label="Image"
+					width="40"
+					@click.native="enlargeImage(props.row.imageUrl)"
+				>
 					<img :alt="props.row.name" :src="props.row.imageUrl" />
 				</b-table-column>
 				<b-table-column field="edit" label="" width="40">
@@ -24,11 +29,18 @@
 					<td></td>
 					<td>{{ character.name }}</td>
 					<td>{{ props.row.origin }}</td>
-					<td class="has-text-centered"><img :src="character.imageUrl" alt="" /></td>
+					<td class="has-text-centered">
+						<img @click.native="enlargeImage(character.imageUrl)" :src="character.imageUrl" alt="" />
+					</td>
 					<td></td>
 				</tr>
 			</template>
 		</b-table>
+		<b-modal @close="$emit('close')" :active.sync="modalIsActive" scroll="keep">
+			<section class="section modal-section has-text-centered">
+				<img class="enlarged-image" :src="imageToEnlarge" alt="enlargedImage" />
+			</section>
+		</b-modal>
 	</section>
 </template>
 
@@ -46,11 +58,32 @@
 	export default class MoeTable extends Vue {
 		@Prop({ required: true, default: [], type: Array })
 		characters!: Array<Character>;
+
+		private modalIsActive: boolean = false;
+
+		private imageToEnlarge: string = "";
+
+		private enlargeImage(url: string): void {
+			this.imageToEnlarge = url;
+			this.modalIsActive = true;
+		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	.sub-character-row {
 		background-color: #202020;
+	}
+	/deep/ .modal-background {
+		background-color: rgba(8, 8, 8, 0.86) !important;
+	}
+	.modal-textarea {
+		width: 100%;
+		height: 100%;
+		background-color: rgb(0, 0, 0) !important;
+		color: #bebebe;
+	}
+	.enlarged-image {
+		max-height: 85vh;
 	}
 </style>
